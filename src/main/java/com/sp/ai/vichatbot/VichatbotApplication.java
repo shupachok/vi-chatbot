f087b215c356c921102f41cdeb745ca9ed507730
SUPACHOK\shupa <shupachok@gmail.com>
2026-04-14 17:36:28 +0700

    add: init peoject

---
+++ src/main/java/com/sp/ai/vichatbot/VichatbotApplication.java
@@ -0,0 +1,13 @@
+package com.sp.ai.vichatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VichatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VichatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sp/ai/vichatbot/VichatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.ai.vichatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VichatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
